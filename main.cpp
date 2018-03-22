9775cb3b8b6f979f56c012d3121648afcf52a89f
Water Aura <siddhatiwari@gmail.com>
2018-03-22 19:19:44 -0400

    Added how to properly end server

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 std::thread::id main_thread_id;
 
+// To properly end server, press ctrl+c in server window or enter "quit" client-side
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
